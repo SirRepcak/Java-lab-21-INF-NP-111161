6a9472a02f618a501a98f3d8327ef489716e5886
SirRepcak <kacperzyberski@gmail.com>
2023-10-16 21:15:04 +0200

    Zadanie 1

--- src/main/java/Lab2/zadanie1.java
+++ src/main/java/Lab2/zadanie1.java
@@ -1,4 +1,23 @@
 package Lab2;
 
+import java.util.Scanner;
+
 public class zadanie1 {
-}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj wysokosc choinki: ");
+        int h = scanner.nextInt();
+        int s;
+
+        for(int i=1,k=0;i<=h;i++,k=0){
+            for(s=1;s<=h-i;s++){
+                System.out.print(" ");
+        }
+            while(k!=2*i-1) {
+                System.out.print("*");
+                k++;
+            }
+            System.out.println("");
+
+    }
+}}
